10819e648aec51ffb5059b60cfd3d33c79ff2d08
rebelbob11 <Hunter.11>
2018-08-15 11:04:34 -0400

    Added HTTP Connection request

--- app/src/main/java/com/example/rebelbob11/httpkotlin/MainActivity.kt
+++ app/src/main/java/com/example/rebelbob11/httpkotlin/MainActivity.kt
@@ -1,18 +1,74 @@
 package com.example.rebelbob11.httpkotlin
 
 import android.content.Context
+import android.net.NetworkInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    val URL = "https://simplifiedcoding.net/demos/marvel/"
+    //4672493
+
+    val NURL = "https://simplifiedcoding.net/demos/marvel/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
-        
+
+        var connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+
+        Log.e("CONN MNGR","CONN INFO"+connectivityManager)
+
+
+        val url = URL(NURL)
+
+        Thread(Runnable {
+
+            try {
+
+                var urlConnection:HttpURLConnection = url.openConnection() as HttpURLConnection
+
+                var inputStream = urlConnection.inputStream
+                var reader = InputStreamReader(inputStream)
+                var bufferedReader = BufferedReader(reader)
+
+                var stringBuilder:StringBuilder = StringBuilder()
+
+                var tempString:String
+                tempString = bufferedReader.toString()
+
+                while (true){
+
+                    stringBuilder.append(tempString)
+                    stringBuilder.append("/n")
+
+                }
+
+                Log.d("STRING BUILDER", ""+stringBuilder.toString())
+
+
+
+            }
+            catch (e:Exception){
+                e.printStackTrace()
+            }
+
+        }).start()
+
+
+
+        //request to open the socket for communication
+
+
+
+
+
     }
 }
